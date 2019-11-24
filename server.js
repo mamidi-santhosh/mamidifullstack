7547ef7e0ff206366bc1f97082f597cd3c3cc4f3
User <m.santhosh911@gmail.com>
2019-11-24 20:58:10 +0530

    DAY 2 commit

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
+connectDB();
+
 app.get("/test", (req, res) => res.send("API Running"));
+
+app.use("/api/users", require("./routes/users"));
+app.use("/api/profile", require("./routes/profile"));
+app.use("/api/post", require("./routes/post"));
+app.use("/api/auth", require("./routes/auth"));
 
 const PORT = process.env.PORT || 5000;
 
